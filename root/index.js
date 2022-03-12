5fb9d8f712a4665c0340eb5a4303663c42a6800e
Alexandra Bouillon <abouillon@monetary-metals.com>
2022-03-12 15:36:45 -0800

    more types

--- root/index.js
+++ root/index.js
@@ -1,1 +1,27 @@
+// Basic Types
 var id = 5;
+var company = 'Stuff Co.';
+var isPublished = true;
+var x = 'Hello';
+var ids = [1, 2, 3, 4];
+var arr = [1, true, 'Hello'];
+// Tuple
+var person = [1, 'Brad', true];
+// Tuple Array
+var employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+];
+// union
+var pid = 22;
+// Enum = sets of named contants
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
--- root/index.ts
+++ root/index.ts
@@ -2,3 +2,37 @@
 let id: number = 5;
 let company:string = 'Stuff Co.';
 let isPublished: boolean = true;
+let x: any = 'Hello';
+let ids:number[] = [1,2,3,4]
+let arr:any[] = [1,true, 'Hello'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true];
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+]
+
+// union
+let pid: string | number = 22;
+
+// Enum = sets of named contants
+enum Direction1{
+    Up = 1,
+    Down = 2,
+    Left = 3,
+    Right = 4
+}
+console.log(Direction1.Left)
+enum Direction2{
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right'
+}
+console.log(Direction2.Left)
